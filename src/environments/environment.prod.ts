9abcb73322e771ddaeb0c84c46c004f55f8755c1
Allain <itsallain@gmail.com>
2022-04-24 11:26:46 +0800

    content changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyA08-amMeAW13v5BPXcLcfuj8d6fgIq5lk",
-  authDomain: "a-con-construction-corporation.firebaseapp.com",
-  projectId: "a-con-construction-corporation",
-  storageBucket: "a-con-construction-corporation.appspot.com",
-  messagingSenderId: "337119008712",
-  appId: "1:337119008712:web:5961ef9207ac17eff692fd",
-  measurementId: "G-DE51KVJN5R"
+  apiKey: "AIzaSyBKdqUB-9aKcgh2zhNSmqFJQf0fMkP4GkQ",
+  authDomain: "a-conbel-builders-corporation.firebaseapp.com",
+  projectId: "a-conbel-builders-corporation",
+  storageBucket: "a-conbel-builders-corporation.appspot.com",
+  messagingSenderId: "984880768707",
+  appId: "1:984880768707:web:0ce5df6bac83bf1ae5e476",
+  measurementId: "G-8XBNSFFXRF"
 };
